33dd8afcf20272357a29a26d744faedad96269ff
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-08-12 08:51:21 -0300

    Adiciona bootstrap navwalker

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,20 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-faded px-0">
+            <button class="navbar-toggler navbar-toggler-right" type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportedContent" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                @if (has_nav_menu('primary_navigation'))
+                {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav mr-auto']) !!}
+                @endif
+            </div>
+        </nav>
+    </div>
 </header>
